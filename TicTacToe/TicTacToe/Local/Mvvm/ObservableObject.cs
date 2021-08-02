77f4f1a94610fb80cd1dd9fb7917574310a717c9
김경훈 <kidi052812@gmail.com>
2021-08-02 15:52:26 +0900

    [ADD] : ObservableObject

---
+++ TicTacToe/TicTacToe/Local/Mvvm/ObservableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace TicTacToe.Local.Mvvm
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
